3ea19e35eb63451b9f7e767b589d90af73403c66
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-09 20:59:07 -0600

    Continuing work on the front page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Lato } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin']
+const lato = Lato({
+    subsets: ['latin'],
+    variable: '--lato-regular' ,
+    weight: '400'
 });
 
 export const metadata: Metadata = {
@@ -18,7 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang='en'>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${lato.variable} antialiased`}
             >
                 {children}
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,59 @@
+import logoText from 'logo.svg'
 import Image from 'next/image';
+import sty from './page.module.css'
 
 export default function Home() {
     return (
-        <div>
-            {/* TODO: Finish the front page design */}
-            <main className='relative flex' id='top_section'>
-                <Image
-                    id='logo'
-                    src='../../public/logo.svg'
-                    alt='Evintli logo'
-                    width={101}
-                    height={37}
-                />
-                <div className='items-center' id='searchbar_and_filter'>
+        <div className='flex flex-row justify-center-safe' id='frame'>
+            <div className='absolute shrink-0 h-[53px] w-[591px] top-0'>
+                <div className='absolute w-[1512px] h-[3px] top-[117px] left-0'>
+                    <div className='absolute w-[591px] h-[53px] top-0 left-[433px]'>
+                        <div className='relative w-[595px] h-[53px]'>
+                            <div className='absolute w-[591px] h-[53px] top-0'>
+                                <div className='relative h-[53px]'>
+                                    <div className='absolute w-[595px] h-[53px] top-0 left-0'>
+                                        <div className={sty.filterIcon} id='filter'>
+                                            {/* Put filter icon here. */}
+                                        </div>
+
+                                        <div className={sty.searchBar} id='search_bar'>
+                                            <div className={`${sty.searchComponent} left-[13px]`} id='search_location'>
+                                                Search Location
+                                            </div>
+                                            <p className={`${sty.searchComponent} left-[221px]`}>
+                                                What are you looking for?
+                                            </p>
+                                            {/* Put search icon here. */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className='absolute w-px h-[35px] top-px left-[198px]'
+                                id='divider'
+                            >
+                                <svg width='1' height='35' viewBox='0 0 1 35' fill='none'>
+                                    <line
+                                        x1='0.5'
+                                        y1='0.5'
+                                        x2='0.499998'
+                                        y2='34.5'
+                                        stroke='#71767C'
+                                        stroke-linecap='round'
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Image
+                        className='absolute w-[101px] h-[37px]'
+                        id='logo_text'
+                        src={logoText}
+                        alt='logo with text'
+                    />
                 </div>
-            </main>
+            </div>
         </div>
     );
 }
