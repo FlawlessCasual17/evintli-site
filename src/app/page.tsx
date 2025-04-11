320f3ece5aea0252bcaa4e96b818774ac3038d38
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-11 16:24:13 -0600

    Added the notifications button and did more reformatting
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import notificationIcon from '../../public/bell.svg'
 import filterIcon from '../../public/filter-icon.svg'
 import searchIcon from '../../public/search-icon.svg'
 import logoText from '../../public/logo.svg'
@@ -7,32 +8,41 @@ import sty from './page.module.css'
 export default function Home() {
     return (
         <div className='flex flex-row justify-center-safe scale-200' id='front_page'>
-            <div className='absolute shrink-0 h-[53px] w-[591px] top-0' id='absolute_shrink0_h53px'>
-                <div className='absolute w-[1512px] h-[3px] top-[117px] left-0' id='absolute_w1512px_h3px'>
-                    <div className='absolute w-[591px] h-[53px] top-0 left-[433px]' id='search_and_filter_1'>
-                        <div className='relative w-[595px] h-[53px]' id='search_and_filter_2'>
-                            <div className='absolute w-[591px] h-[53px] top-0' id='search_and_filter_3'>
-                                <div className='relative h-[53px]' id='search_and_filter_4'>
-                                    <div className='absolute w-[595px] h-[53px] top-0 left-0' id='filter__and__search_bar'>
-                                        <div className={sty.addListing} id='add_listing'>
-                                            <div>Add Listing</div>
-                                        </div>
-
+            <div className='absolute h-[53px] w-[591px]' id='absolute_shrink0_h53px'>
+                <div className='absolute w-[1512px] h-[3px] top-[117px]' id='absolute_w1512px_h3px'>
+                    <div className='absolute w-[591px] h-[53px] left-[433px]' id='topsection_1'>
+                        <div className='relative w-[595px] h-[53px]' id='topsection_2'>
+                            <div className='absolute w-[591px] h-[53px]' id='topsection_3'>
+                                <div className='relative h-[53px]' id='topsection_4'>
+                                    <div className={`${sty.notification} shadow-effect`} id='notifications'>
                                         <Image
-                                            className='absolute top-1 left-[34rem] w-[43px] h-[45px]'
+                                            className='relative top-2 left-[9px]'
+                                            id='bell'
+                                            src={notificationIcon}
+                                            alt='bell'
+                                        />
+                                    </div>
+
+                                    <div className={sty.addListing} id='add_listing'>
+                                        <div>Add Listing</div>
+                                    </div>
+
+                                    <div className='absolute w-[595px] h-[53px]' id='filter_search_bar'>
+                                        <Image
+                                            className='absolute top-1 left-[34rem]'
                                             id='filter'
                                             src={filterIcon}
                                             alt='filter'
                                         />
 
                                         <Image
-                                            className='absolute w-[43px] h-11 top-1 left-[491px]'
+                                            className='absolute h-11 top-1 left-[491px]'
                                             id='search_icon'
                                             src={searchIcon}
                                             alt='search'
                                         />
 
-                                        <div className={`${sty.searchBar} shadow-[0px_0px_4px_#00000040]`} id='search_bar'>
+                                        <div className={`${sty.searchBar} shadow-effect`} id='search_bar'>
                                             <div className='relative flex flex-row' id='sub_headers'>
                                                 <div className={`${sty.subHeader} left-8`} id='where_sub_header'>
                                                     Where?
@@ -46,7 +56,7 @@ export default function Home() {
                                                 <div className={`${sty.description} left-0.5`} id='where_description'>
                                                     Search Location
                                                 </div>
-                                                <div className={`${sty.description}  left-[66px]`} id='search_description'>
+                                                <div className={`${sty.description} left-[66px]`} id='search_description'>
                                                     What are you looking for?
                                                 </div>
                                             </div>
@@ -74,7 +84,7 @@ export default function Home() {
                         className='absolute w-[101px] h-[37px]'
                         id='logo_text'
                         src={logoText}
-                        alt='logo of Evintli'
+                        alt='logo'
                     />
                 </div>
             </div>
