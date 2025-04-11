c5d2687c2de665e89b64f051b5edf0e6090ee9a4
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-11 15:31:05 -0600

    Fixed styling error on the searchbar and finished adding the "Add Listing" button
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,53 +13,59 @@ export default function Home() {
                         <div className='relative w-[595px] h-[53px]' id='search_and_filter_2'>
                             <div className='absolute w-[591px] h-[53px] top-0' id='search_and_filter_3'>
                                 <div className='relative h-[53px]' id='search_and_filter_4'>
-                                    <div className='absolute w-[595px] h-[53px] top-0 left-0' id='filter_icon__and__search_bar'>
-                                        <div className='absolute w-[43px] h-[45px]' id='filter'>
-                                            <Image
-                                                className='absolute top-1 left-[34rem]'
-                                                id='filter_icon'
-                                                src={filterIcon}
-                                                alt='filters'
-                                            />
+                                    <div className='absolute w-[595px] h-[53px] top-0 left-0' id='filter__and__search_bar'>
+                                        <div className={sty.addListing} id='add_listing'>
+                                            <div>Add Listing</div>
                                         </div>
 
-                                        <div className={sty.searchBar} id='search_descriptions'>
-                                            <div className={`${sty.searchComponent} left-[13px]`}>
-                                                Search Location
+                                        <Image
+                                            className='absolute top-1 left-[34rem] w-[43px] h-[45px]'
+                                            id='filter'
+                                            src={filterIcon}
+                                            alt='filter'
+                                        />
+
+                                        <Image
+                                            className='absolute w-[43px] h-11 top-1 left-[491px]'
+                                            id='search_icon'
+                                            src={searchIcon}
+                                            alt='search'
+                                        />
+
+                                        <div className={`${sty.searchBar} shadow-[0px_0px_4px_#00000040]`} id='search_bar'>
+                                            <div className='relative flex flex-row' id='sub_headers'>
+                                                <div className={`${sty.subHeader} left-8`} id='where_sub_header'>
+                                                    Where?
+                                                </div>
+                                                <div className={`${sty.subHeader} left-36`} id='search_sub_header'>
+                                                    Search
+                                                </div>
                                             </div>
-                                            <div className={`${sty.searchComponent} left-[221px]`}>
-                                                What are you looking for?
+
+                                            <div className='relative flex flex-row top-4' id='descriptions'>
+                                                <div className={`${sty.description} left-0.5`} id='where_description'>
+                                                    Search Location
+                                                </div>
+                                                <div className={`${sty.description}  left-[66px]`} id='search_description'>
+                                                    What are you looking for?
+                                                </div>
                                             </div>
-                                            <Image
-                                                className='absolute w-[43px] h-11 top-1 left-[491px]'
-                                                id='search_icon'
-                                                src={searchIcon}
-                                                alt='search'
-                                            />
                                         </div>
-                                    </div>
 
-                                    <div className={`${sty.subHeader} left-[42.5px]`} id='where_sub_header'>
-                                        Where?
-                                    </div>
-
-                                    <div className={`${sty.subHeader} text-center left-[204px]`} id='search_sub_header'>
-                                        Search
+                                        <div className='absolute w-px h-[35px] top-2 left-[198px]' id='divider'>
+                                            <svg width='1' height='35' viewBox='0 0 1 35' fill='none'>
+                                                <line
+                                                    x1='0.5'
+                                                    y1='0.5'
+                                                    x2='0.499998'
+                                                    y2='34.5'
+                                                    stroke='#71767C'
+                                                    strokeLinecap='round'
+                                                />
+                                            </svg>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-
-                            <div className='absolute w-px h-[35px] top-2 left-[198px]' id='divider'>
-                                <svg width='1' height='35' viewBox='0 0 1 35' fill='none'>
-                                    <line
-                                        x1='0.5'
-                                        y1='0.5'
-                                        x2='0.499998'
-                                        y2='34.5'
-                                        stroke='#71767C'
-                                        strokeLinecap='round'
-                                    />
-                                </svg>
                             </div>
                         </div>
                     </div>
